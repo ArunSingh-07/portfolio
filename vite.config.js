21cca793a714c15b15044c604a6c92f19d35f9d5
Arun Singh <arun987770@gmail.com>
2023-06-16 08:50:32 +0530

    add: workflows deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
-    plugins: [react()] 
+    plugins: [react()],
+    base: '/', 
   }
+
+  if (command !== 'serve') {
+    config.base = '/portfolio/'
+  }
+  
   return config
 })
